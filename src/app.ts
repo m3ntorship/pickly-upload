f22d2b7d8ba036d5142406bb69b5d69962458e47
seifeldeen3388 <seifeldeen3388@gmail.com>
2020-11-10 16:57:39 +0200

    added multer config in a separate file

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,11 @@
-import express, { Application, Request } from 'express';
-import multer from 'multer';
+import express, { Application} from 'express';
+import { upload } from './multer';
 
 const app: Application = express();
 
-const fileFilter = (
-	req: Request,
-	file: Express.Multer.File,
-	cb: multer.FileFilterCallback
-) => {
-	const acceptedMimeTypes = ['image/jpg', 'image/jpeg', 'image/png'];
-	if (acceptedMimeTypes.includes(file.mimetype)) {
-		cb(null, true);
-	} else {
-		cb(
-			new Error(
-				'please upload an image with one of the accepted formats: "image/jpg", "image/jpeg", "image/png" '
-			)
-		);
-	}
-};
-
-const upload: multer.Multer = multer({ fileFilter });
-app.use(upload.array('options', 4));
+app.use(upload.array('options', 4), (req, res, next) => {
+	res.status(404).send();
+});
 app.use(express.json());
 
 export default app;
---
+++ src/multer.ts
@@ -0,0 +1,21 @@
+import multer from 'multer';
+import { Request } from 'express';
+
+const fileFilter = (
+	req: Request,
+	file: Express.Multer.File,
+	cb: multer.FileFilterCallback
+) => {
+	const acceptedMimeTypes = ['image/jpg', 'image/jpeg', 'image/png'];
+	if (acceptedMimeTypes.includes(file.mimetype)) {
+		cb(null, true);
+	} else {
+		cb(
+			new Error(
+				'please upload an image with one of the accepted formats: "image/jpg", "image/jpeg", "image/png" '
+			)
+		);
+	}
+};
+
+export const upload: multer.Multer = multer({ fileFilter: fileFilter });
