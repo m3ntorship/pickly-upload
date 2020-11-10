4f9a46d8ee2cde3a40be80999c8672a6e62e64e9
seifeldeen3388 <seifeldeen3388@gmail.com>
2020-11-10 15:38:25 +0200

    initial set up for multer - added providers files

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,27 @@
-import express, { Application } from 'express';
+import express, { Application, Request } from 'express';
+import multer from 'multer';
 
 const app: Application = express();
 
+const fileFilter = (
+	req: Request,
+	file: Express.Multer.File,
+	cb: multer.FileFilterCallback
+) => {
+	const acceptedMimeTypes = ['image/jpg', 'image/jpeg', 'image/png'];
+	if (acceptedMimeTypes.includes(file.mimetype)) {
+		cb(null, true);
+	} else {
+		cb(
+			new Error(
+				'please upload an image with one of the accepted formats: "image/jpg", "image/jpeg", "image/png" '
+			)
+		);
+	}
+};
+
+const upload: multer.Multer = multer({ fileFilter });
+app.use(upload.array('options', 4));
 app.use(express.json());
 
 export default app;
