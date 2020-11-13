e22f53585da3d071bbf9425792e78e2eb8226237
seifeldeen3388 <seifeldeen3388@gmail.com>
2020-11-13 14:17:24 +0200

    successfully uploaded on imgur

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,27 @@
 import express, { Application } from 'express';
 import { upload_imgur } from './upload/providers/imgur';
+import { Client } from '@rmp135/imgur';
+import config from 'config';
 
 const app: Application = express();
 
-app.use(upload_imgur('options', 4), (req, res, next) => {
-	res.status(404).send();
+const client_id: string = config.get('upload_providers.imgur.client_id');
+const client_secret: string = config.get(
+	'upload_providers.imgur.client_secret'
+);
+let client = new Client({
+	client_id,
+	client_secret,
+});
+
+client.Image.upload;
+app.use(upload_imgur('options'), async (req, res, next) => {
+	try {
+		const apiRes = await client.Image.upload(req.file.buffer);
+		res.status(200).json({ apiRes });
+	} catch (error) {
+		res.status(404).json({ error });
+	}
 });
 
 app.use(express.json());
--- src/upload/providers/imgur.ts
+++ src/upload/providers/imgur.ts
@@ -2,25 +2,15 @@ import multer, { diskStorage, memoryStorage } from 'multer';
 import config from 'config';
 import { fileFilter } from '../multer';
 import { Request, RequestHandler } from 'express';
-import imgur from "imgur";
-
-const client_id: string = config.get('upload_providers.imgur.client_id');
-const client_secret: string = config.get(
-	'upload_providers.imgur.client_secret'
-);
-
-// const client = new Client({
-// 	client_id,
-// 	client_secret,
-// });
 
 export const upload_imgur: (
 	fieldName: string,
 	maxCount?: number
-) => RequestHandler = (fieldName, maxCount) => {
+) => RequestHandler = (fieldName) => {
 	const upload = multer({
 		fileFilter,
+		storage: memoryStorage(),
 	});
-	return upload.array(fieldName, maxCount);
+	return upload.single(fieldName);
 	// return client.Image.upload(`${__dirname}/temp`);
 };
