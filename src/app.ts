1f74b1f61eb4af244125f625dee5d878db0f3f2e
seifeldeen3388 <seifeldeen3388@gmail.com>
2020-11-13 23:05:36 +0200

    exported multer upload only instead of request handler

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,11 @@ import express, { Application } from 'express';
 import { upload_imgur } from './upload/providers/imgur/upload';
 const app: Application = express();
 
-app.use(upload_imgur('options', 4), (req, res, next) => {
-		const optionsData = (req.files as Array<Express.Multer.File>).map(
-			(option: any) => option.data
-		);
-		res.status(200).json({ optionsData });
+app.use(upload_imgur.array('options', 4), (req, res, next) => {
+	const optionsData = (req.files as Array<Express.Multer.File>).map(
+		(option: any) => option.data
+	);
+	res.status(200).json({ optionsData });
 });
 
 app.use(express.json());
--- src/upload/providers/imgur/upload.ts
+++ src/upload/providers/imgur/upload.ts
@@ -1,16 +1,9 @@
 import multer from 'multer';
-import { RequestHandler } from 'express';
 import { fileFilter } from '../../multer';
 import { imgurStorage } from './storageEngine';
 import { client } from './client';
 
-export const upload_imgur: (
-	fieldName: string,
-	maxNum: number
-) => RequestHandler = (fieldName, maxNum) => {
-	const upload = multer({
-		fileFilter,
-		storage: imgurStorage(client),
-	});
-	return upload.array(fieldName, maxNum);
-};
+export const upload_imgur: multer.Multer = multer({
+	fileFilter,
+	storage: imgurStorage(client),
+});
