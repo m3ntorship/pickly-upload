80437602cdd58cddf960c55347b4b9d37a7bf335
seifeldeen3388 <seifeldeen3388@gmail.com>
2020-11-13 20:29:35 +0200

    removed unnecessary try catch

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,10 @@ import { upload_imgur } from './upload/providers/imgur/upload';
 const app: Application = express();
 
 app.use(upload_imgur('options', 4), (req, res, next) => {
-	try {
 		const optionsData = (req.files as Array<Express.Multer.File>).map(
 			(option: any) => option.data
 		);
 		res.status(200).json({ optionsData });
-	} catch (error) {
-		res.status(404).json({ error });
-	}
 });
 
 app.use(express.json());
